507a6f88b3f6818629345e0425b530f2c8d8cd1f
Milos Adnadjevic <milos@Miloss-Air.lan>
2026-01-03 11:06:37 -0500

    updated UI

--- ai-trainer-frontend/src/components/Header.jsx
+++ ai-trainer-frontend/src/components/Header.jsx
@@ -1,13 +1,12 @@
 import whiteLogo from "../assets/logo.svg";
-import blueLogo from '../assets/purple.svg';
 
-const Header = ({ isDarkMode }) => {
+const Header = () => {
   return (
     <div className="flex justify-center">
       <img
-        src={isDarkMode ? whiteLogo : blueLogo}
+        src={whiteLogo}
         alt="logo"
-        className="w-36 sm:w-40 drop-shadow-[0_10px_30px_rgba(255,255,255,0.15)]"
+        className="w-36 sm:w-40 drop-shadow-[0_10px_30px_rgba(0,0,0,0.3)]"
       />
     </div>
   );
